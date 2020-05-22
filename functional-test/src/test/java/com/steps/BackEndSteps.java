df7c8a36d17cb88353dbea24480945d163244bda
Paola Duquino <angela.duquino@appgate.com>
2020-05-22 12:01:16 -0500

    Implement

--- functional-test/src/test/java/com/steps/BackEndSteps.java
+++ functional-test/src/test/java/com/steps/BackEndSteps.java
@@ -4,31 +4,35 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import cucumber.api.java.en.*;
+import org.junit.Assert;
 
 public class BackEndSteps {
 
-        String URL;
+    String URL;
+    HttpResponse response;
 
-        @Given("^the service: (.*)$")
-        public void setService (String url)  {
-            System.out.println("The service: " + url);
-            URL = url;
-        }
+    @Given("^the service: (.*)$")
+    public void setService(String url) {
+        System.out.println("The service: " + url);
+        URL = url;
+    }
 
-        @When("^the user does a (.*) petition$")
-        public void callService (String petition) throws UnirestException {
-            System.out.println("The user does a " + petition + " petition");
-            HttpResponse response = Unirest.get(URL)
-                    .header("Accept", "application/json")
-                    .asString();
-            System.out.println("The Response is: " + response);
-            System.out.println("The Body is: " + response.getBody());
-            System.out.println("The Status Code is: " + response.getStatus());
-        }
+    @When("^the user does a (.*) petition$")
+    public void callService(String petition) throws UnirestException {
+        System.out.println("The user does a " + petition + " petition");
+        response = Unirest.get(URL)
+                .header("Accept", "application/json")
+                .asString();
+        System.out.println("The Response is: " + response);
+        System.out.println("The Body is: " + response.getBody());
+    }
 
-        @Then("^the response is (\\d+)$")
-        public void getResponse (Integer code) {
-            System.out.println("The response is " + code);
-        }
+    @Then("^the response is (\\d+)$")
+    public void getResponse(Integer code) {
+        System.out.println("The Status Code is: " + response.getStatus());
+        Integer codeResponse = response.getStatus();
+        Assert.assertEquals(code, codeResponse);
+        System.out.println("The response is " + code);
+    }
 
 }
